b983a64e9832342d61b5187e31f66d2e022a53f1
Samnasamna <samna7634@gmail.com>
2025-03-12 16:49:50 +0530

    dot env installiation deleted

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,1 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-export const BASE_URL = `${import.meta.env.REACT_APP_API_URL}/api/v1`
+export const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`
