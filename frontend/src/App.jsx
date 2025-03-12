4bc00c80c7af96410d62565a0b5e21cea817ff81
Samnasamna <samna7634@gmail.com>
2025-03-12 17:24:55 +0530

    vercel.json added and basename is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Router>
-      <Routes>
+      <Routes basename="/">
         <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
         <Route path="/dashboard" element={token ? <Home /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
